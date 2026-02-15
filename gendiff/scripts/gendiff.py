e3c36a385def4790de5432188f6b59c5c58ae49e
Александр Твинков <s.alex06.a@gmail.com>
2026-02-15 23:43:53 +0300

    feat: add --format option and configure package build
    - Add -f/--format argument to CLI
    - Configure hatchling build with correct packages
    - Update help message with format option
    - Test package building

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,10 +7,15 @@ def main():
     )
     parser.add_argument('first_file', type=str, help='first file')
     parser.add_argument('second_file', type=str, help='second file')
+    parser.add_argument('-f', '--format', type=str, 
+                        default='stylish',  # значение по умолчанию
+                        help='set format of output (default: stylish)')
     
     args = parser.parse_args()
-    # Пока просто выводим аргументы, потом здесь будет логика сравнения
-    print(f'Comparing {args.first_file} and {args.second_file}')
+    
+    # Пока просто выводим информацию
+    print(f"Comparing {args.first_file} and {args.second_file}")
+    print(f"Output format: {args.format}")
 
 
 if __name__ == '__main__':
