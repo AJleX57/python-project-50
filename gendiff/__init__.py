0b45326fef6cdb8af1597a060cff67dad5933248
AJleX57 <s.alex06.a@gmail.com>
2026-03-23 16:59:06 +0300

    style: fix linter errors and exclude build dir

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,10 +1,9 @@
 from gendiff.core.parser import parse_file
-from gendiff.formatters import apply_format
 from gendiff.core.diff_tree import build_diff
+from gendiff.formatters import apply_format
 
 
-
-def generate_diff(file_path1: str, file_path2: str, format_name='stylish') -> str:
+def generate_diff(file_path1, file_path2, format_name='stylish'):
     data1 = parse_file(file_path1)
     data2 = parse_file(file_path2)
     diff = build_diff(data1, data2)
--- tests/test_gendiff_nested.py
+++ tests/test_gendiff_nested.py
@@ -7,9 +7,11 @@ def get_fixture_path(file_name):
     current_dir = os.path.dirname(os.path.abspath(__file__))
     return os.path.join(current_dir, 'test_data', file_name)
 
+
 def read_file(file_path):
     with open(file_path, 'r') as f:
         return f.read()
+
 
 def get_expected_result():
     expected_path = os.path.join(
@@ -19,6 +21,7 @@ def get_expected_result():
     )
     return read_file(expected_path).rstrip()
 
+
 @pytest.mark.parametrize("file1,file2", [
     ("nested_file1.json", "nested_file2.json"),
     ("nested_file1.yml", "nested_file2.yml"),
@@ -26,17 +29,14 @@ def get_expected_result():
 def test_generate_diff_nested(file1, file2):
     path1 = get_fixture_path(file1)
     path2 = get_fixture_path(file2)
-
     expected = get_expected_result()
     result = generate_diff(path1, path2)
-
     assert result == expected
+
 
 def test_generate_diff_nested_mixed_formats():
     json_file = get_fixture_path('nested_file1.json')
     yaml_file = get_fixture_path('nested_file2.yml')
-
     expected = get_expected_result()
     result = generate_diff(json_file, yaml_file)
-
     assert result == expected
